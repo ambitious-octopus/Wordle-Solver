61ea422d46319f8082866f0e21daaca3345dc9ac
Francesco Mattioli <Francesco.mttl@gmail.com>
2022-01-22 13:49:33 +0100

    added new strategy

--- solver.py
+++ solver.py
@@ -65,11 +65,77 @@ def filter_by_presence(present_list, absent_list, words):
 
     return candidates
 
+def filter_by_presence_and_position(present_list, absent_list,position, words):
+    candidates = list()
+    for w in words:
+        if all(letter in w for letter in present_list):
+            candidates.append(w)
+        else:
+            pass
+
+    for w in candidates:
+        if any(letter in w for letter in absent_list):
+            candidates.remove(w)
+        else:
+            pass
+
+    regex = ""
+    for letter in position:
+        if letter != "":
+            regex += letter
+        else:
+            regex += "."
+
+    for w in candidates:
+        if re.match(regex, w):
+            pass
+        else:
+            candidates.remove(w)
+
+
+    return candidates
+
+
+
+def the_take_into_account_position_strategy():
+    web = Website()
+
+    # This is a strong combination found somewhere!
+    guess = ['quick', 'brown', 'shady', 'cleft', 'gimps']
+
+    vocab = words
+    present_list = list()
+    absence_list = list()
+    position = ["","","","",""]
+    for i in range(6):
+        if i == 5:
+            print(vocab[0])
+            web.send_word_get_answer(vocab[0])
+            time.sleep(10)
+        elif len(vocab) == 1:
+            web.send_word_get_answer(vocab[0])
+            time.sleep(10)
+        else:
+            round_ = web.send_word_get_answer(guess[i])
+            for index, key_value in enumerate(round_[1].items()):
+                if key_value[1] == "correct":
+                    position[index] = key_value[0]
+                if key_value[1] == "correct" or key_value[1] == "present":
+                    present_list.append(key_value[0])
+                else:
+                    absence_list.append(key_value[0])
+
+            vocab = filter_by_presence_and_position(present_list, absence_list,position, vocab)
+
+            print("Current vocabulary: ")
+            print(vocab)
+
+        time.sleep(2)
+    time.sleep(10)
 
 
 def simple_strategy():
     web = Website()
-
     # This is a strong combination found somewhere!
     guess = ['quick', 'brown', 'shady', 'cleft', 'gimps']
 
@@ -101,7 +167,8 @@ def simple_strategy():
     time.sleep(10)
 
 
-simple_strategy()
 
+# simple_strategy()
+the_take_into_account_position_strategy()
 
 
